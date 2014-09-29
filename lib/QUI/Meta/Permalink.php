636811d66ab355a8bbe59ac3c875f8f44895dee7
Henning Leutz <leutz@pcsg.de>
2014-09-29 08:44:46 +0200

    meta quiqqer milk

---
+++ lib/QUI/Meta/Permalink.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file contains \QUI\Meta\Permalink
+ */
+
+namespace QUI\Meta;
+
+/**
+ * Permalink class
+ *
+ * @author www.pcsg.de (Henning Leutz)
+ */
+
+class Permalink
+{
+
+}
